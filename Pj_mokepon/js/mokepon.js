32c8072e16b277baa2eeb42b958ed0896aab584f
sergiofishtank <sergio.suaf@hotmail.com>
2025-04-01 21:19:59 -0500

    creacion de archivo js y funcion iniciarJuego

---
+++ Pj_mokepon/js/mokepon.js
@@ -0,0 +1,11 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota");
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);    
+}
+
+function seleccionarMascotaJugador() {
+    alert("Seleccionaste tu mascota");
+}
+
+
+window.addEventListener("load", iniciarJuego);
